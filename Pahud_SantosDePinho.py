a62e2ded879e16fc6a788468ba2351a46e81a806
Cédric Pahud <cedric.pahud@he-arc.ch>
2017-11-08 14:43:00 +0100

    created my pycharm project

--- Pahud_SantosDePinho.py
+++ Pahud_SantosDePinho.py
@@ -1,4 +1,13 @@
 import pygame
 
+
+class City:
+
+    def __init__(self,name,x,y):
+        self.name = name
+        self.x = x
+        self.y = y
+
+
 def ga_solve(file=None, gui=True, maxtime=0):
     print("ga_solve")
