62b58617ee71baee0d938fcc9c905d67b46d8353
Cédric Pahud <cedric.pahud@he-arc.ch>
2017-11-15 14:42:34 +0100

    structure et fitness

--- Pahud_SantosDePinho.py
+++ Pahud_SantosDePinho.py
@@ -2,9 +2,41 @@ import pygame
 from pygame.locals import KEYDOWN, QUIT, MOUSEBUTTONDOWN, K_RETURN, K_ESCAPE
 from collections import namedtuple
 import sys
+from random import shuffle
 
 
 City = namedtuple('City', 'name,x,y')
+
+
+class Individual:
+
+    def __init__(self, cities):
+        self.cities = cities
+        print(self.cities)
+
+    def mutate(self):
+        pass
+
+    def cross_breeding(self,individual):
+        pass
+
+    def fitness(self):
+        value = 0
+        for i, city in enumerate(self.cities):
+            value += abs(city.x-city[i-1].x)+abs(city.y-city[i-1].y)
+        return value
+
+
+class Population:
+
+    def __init__(self, size, mutationRate, crossRate, cities):
+        self.individuals = []
+        self.size = size
+        self.mutationRate = mutationRate
+        self.crossRate = crossRate
+        for i in range(size):
+            shuffle(cities)
+            self.individuals.append(Individual(cities))
 
 
 def read_from_gui():
@@ -46,7 +78,7 @@ def read_from_gui():
             elif event.type == MOUSEBUTTONDOWN:
                 x, y = pygame.mouse.get_pos()
                 cities.append(City(f"v{cityCounter}", x, y))
-                cityCounter+=1
+                cityCounter += 1
                 draw(cities)
 
     return cities
@@ -67,6 +99,8 @@ def ga_solve(file=None, gui=True, maxtime=0):
         cities = read_from_gui()
     else:
         cities = reading_from_file(file)
+
+    pop = Population(10, 0.001, 7, cities)
 
 
 if __name__ == "__main__":
